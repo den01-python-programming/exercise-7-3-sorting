8b6e7476546f21767d485562d5bb1e90c9dfca67
Scott Morgan <smorgan@bridgend.ac.uk>
2020-05-13 15:22:59 +0100

    Update test_exercise.py

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,5 +1,5 @@
 import pytest
-from src.exercise import smallest, index_of_smallest, index_of_smallest_from, swap, sort
+from src.exercise import smallest, index_of_smallest, index_of_smallest_from, swap, sort_numbers
 
 def test_exercise():
     numbers = [6, 5, 8, 7, 11]
@@ -8,10 +8,12 @@ def test_exercise():
     assert index_of_smallest(numbers) == 1
     assert index_of_smallest_from(numbers, 2) == 3
 
+    numbers = [6, 5, 8, 7, 11]
+
     swap(numbers, 1, 0)
 
     assert numbers == [5, 6, 8, 7, 11]
 
-    sort(numbers)
+    sort_numbers(numbers)
 
     assert numbers == [5, 6, 7, 8, 11]
